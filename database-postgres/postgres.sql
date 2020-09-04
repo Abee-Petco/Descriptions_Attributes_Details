8ad3c906c4936496660c63d52be23c4410f3974e
Sam Johnson <samj@iconcollective.com>
2020-09-04 10:14:19 -0500

    broke colors and brands tables out from descriptions.  issues with Item.csv generation need bugfix.

--- database-postgres/postgres.sql
+++ database-postgres/postgres.sql
@@ -3,12 +3,22 @@ CREATE TABLE descriptions (
   title VARCHAR(50),
   description VARCHAR(500),
   sku INT,
-  primaryBrand VARCHAR(50),
+  brand_id INT,
   daysToShip VARCHAR(50),
   directions VARCHAR(500),
-  primaryColor VARCHAR(25),
+  color_id INT,
   material VARCHAR(25),
   length VARCHAR(10),
   width VARCHAR(10),
   additionalDetails VARCHAR(500)
+);
+
+CREATE TABLE colors (
+  colorId INT PRIMARY KEY,
+  primaryColor VARCHAR(25)
+);
+
+CREATE TABLE brands (
+  brandId INT PRIMARY KEY,
+  primaryBrand VARCHAR(50)
 );
