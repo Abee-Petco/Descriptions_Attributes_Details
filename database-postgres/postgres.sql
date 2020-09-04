90e225e2682af51cf1458b9097000e289f41d642
Sam Johnson <samj@iconcollective.com>
2020-09-03 23:40:20 -0500

    simplified and seeded SDC.csv into postgres db and created generator for postgres

--- database-postgres/postgres.sql
+++ database-postgres/postgres.sql
@@ -1,42 +1,14 @@
 CREATE TABLE descriptions (
-  id int NOT NULL,
+  itemId INT PRIMARY KEY,
   title VARCHAR(50),
-  description VARCHAR(400),
-  sku int NOT NULL,
+  description VARCHAR(500),
+  sku INT,
   primaryBrand VARCHAR(50),
   daysToShip VARCHAR(50),
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE directions (
-  id int NOT NULL,
-  directions VARCHAR(150),
-  description_id int NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (description_id)
-    REFERENCES descriptions (id)
-    ON DELETE CASCADE
-);
-
-CREATE TABLE attributes (
-  id int NOT NULL,
+  directions VARCHAR(500),
   primaryColor VARCHAR(25),
   material VARCHAR(25),
   length VARCHAR(10),
   width VARCHAR(10),
-  description_id int NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (description_id)
-    REFERENCES descriptions (id)
-    ON DELETE CASCADE
-);
-
-CREATE TABLE details (
-  id int NOT NULL,
-  additionalDetails VARCHAR(400),
-  description_id int NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (description_id)
-    REFERENCES descriptions (id)
-    ON DELETE CASCADE
+  additionalDetails VARCHAR(500)
 );
