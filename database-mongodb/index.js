a5d0ed67bd91a22eaae9ca7d552594fb9844ad15
Sam Johnson <samj@iconcollective.com>
2020-09-11 15:01:09 -0500

    fixed SKU to use String again

--- database-mongodb/index.js
+++ database-mongodb/index.js
@@ -43,7 +43,7 @@ const descriptionSchema = new mongoose.Schema({
     required: true,
   },
   description: String,
-  SKU: Number,
+  SKU: String,
   primaryBrand: String,
   daysToShip: String,
 });
