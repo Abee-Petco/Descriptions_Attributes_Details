365c20afd90ea13dc2c8d98f48ee7978fc7afe79
Sam Johnson <samj@iconcollective.com>
2020-09-15 01:35:37 -0500

    moved endpoint test mocks to separate file

---
+++ serverMocks.js
@@ -0,0 +1,45 @@
+module.exports.postData = {
+  itemId: '66',
+  description: {
+    title: 'Fear and Loathing',
+    description: 'Once upon a time in the city of Los Angeles',
+    SKU: '6666666',
+    'Primary Brand': 'Gucci',
+    daysToShip: 'Ships in 365 business days',
+  },
+  directions: {
+    directions: 'Just a toot past the watering hole',
+  },
+  attributes: {
+    primaryColor: 'coral',
+    material: 'Melted',
+    length: '7 IN',
+    width: '7 IN',
+  },
+  details: {
+    additionalDetails: 'Loves long walks on the beach',
+  },
+};
+
+module.exports.putData = {
+  itemId: '66',
+  description: {
+    title: 'Dead Astronauts',
+    description: 'Once upon a time in the city of Los Angeles',
+    SKU: '6666666',
+    'Primary Brand': 'Gucci',
+    daysToShip: 'Ships in 365 business days',
+  },
+  directions: {
+    directions: 'Just a toot past the watering hole',
+  },
+  attributes: {
+    primaryColor: 'coral',
+    material: 'Melted',
+    length: '7 IN',
+    width: '7 IN',
+  },
+  details: {
+    additionalDetails: 'Loves long walks on the beach',
+  },
+};
