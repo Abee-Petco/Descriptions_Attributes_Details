38c7c9696954d42bd2be2a89ad9f99b0af205477
Sam Johnson <samj@iconcollective.com>
2020-09-15 01:25:01 -0500

    all mockgoose tests passing again

--- database-mongodb/index.spec.js
+++ database-mongodb/index.spec.js
@@ -1,105 +1,148 @@
 const mongoose = require('mongoose');
 const db = require('./index.js');
-const Mockgoose = require('mockgoose').Mockgoose;
-const mockgoose = new Mockgoose(mongoose);
 
 const postData = {
-  itemId: '50',
-  title: 'Dead Astronauts',
-  description: 'Once upon a time in the city of Los Angeles',
-  SKU: '6666666',
-  primaryBrand: 'Gucci',
-  daysToShip: 'Ships in 365 business days',
-  directions: 'Just a toot past the watering hole',
-  primaryColor: 'coral',
-  material: 'Melted',
-  length: '7 IN',
-  width: '7 IN',
-  additionalDetails: 'Loves long walks on the beach',
+  itemId: 50,
+  description: {
+    title: 'Dead Astronauts',
+    description: 'Once upon a time in the city of Los Angeles',
+    SKU: 6666666,
+    primaryBrand: 'Gucci',
+    daysToShip: 'Ships in 365 business days',
+  },
+  directions: {
+    directions: 'A left on Morose and right on Texas',
+  },
+  attributes: {
+    primaryColor: 'coral',
+    material: 'Melted',
+    length: '7 IN',
+    width: '7 IN',
+  },
+  details: {
+    additionalDetails: 'Loves long walks on the beach',
+  },
 };
 
-const putData = {"title": "TheShining"};
+const postRes = {
+  description: {
+    title: 'Dead Astronauts',
+    description: 'Once upon a time in the city of Los Angeles',
+    SKU: 6666666,
+    primaryBrand: 'Gucci',
+    daysToShip: 'Ships in 365 business days',
+  },
+  directions: {
+    directions: 'A left on Morose and right on Texas',
+  },
+  attributes: {
+    primaryColor: 'coral',
+    material: 'Melted',
+    length: '7 IN',
+    width: '7 IN',
+  },
+  details: {
+    additionalDetails: 'Loves long walks on the beach',
+  },
+};
+
+const putData = {
+  description: {
+    title: 'The Shining',
+    description: 'Once upon a time in the city of Los Angeles',
+    SKU: 6666666,
+    primaryBrand: 'Gucci',
+    daysToShip: 'Ships in 365 business days',
+  },
+};
 
 const putResult = {
-  itemId: '50',
-  title: 'The Shining',
-  description: 'Once upon a time in the city of Los Angeles',
-  SKU: '6666666',
-  primaryBrand: 'Gucci',
-  daysToShip: 'Ships in 365 business days',
-  directions: 'Just a toot past the watering hole',
-  primaryColor: 'coral',
-  material: 'Melted',
-  length: '7 IN',
-  width: '7 IN',
-  additionalDetails: 'Loves long walks on the beach',
+  description: {
+    title: 'The Shining',
+    description: 'Once upon a time in the city of Los Angeles',
+    SKU: 6666666,
+    primaryBrand: 'Gucci',
+    daysToShip: 'Ships in 365 business days',
+  },
+  directions: {
+    directions: 'A left on Morose and right on Texas',
+  },
+  attributes: {
+    primaryColor: 'coral',
+    material: 'Melted',
+    length: '7 IN',
+    width: '7 IN',
+  },
+  details: {
+    additionalDetails: 'Loves long walks on the beach',
+  },
 };
 
-
 describe('Mongoose', () => {
-  // beforeEach(async (done) => {
-  //   mockgoose.helper.reset().then(() => {
-  //     done()
-  // });
-  // })
   afterAll(() => {
-    mongoose.connection.close()
-  })
-  xit('should have methods for retrieving item docs', async (done) => {
-    expect(db.getTitleAndBrand).toBeDefined()
-    expect(db.getDescriptionObject).toBeDefined()
-    
-    db.getDescriptionObject(105)
-      .then(data => {
-        expect(data[0].title).toBe('IpsumLorem Plush Bunny with Squeaker');
-        expect(data[0].itemId).toBe('105');
-        done()
-      }) 
-    })
-    it('should have methods for posting item docs', async (done) => {
-      expect(db.postDescriptionObject).toBeDefined()
-      
-      db.postDescriptionObject(postData)
-      .then(data => {
-        expect(data.itemId).toBe('50');
+    mongoose.connection.close();
+  });
+  it('should have methods for retrieving item docs and their subdocs', async (done) => {
+    expect(db.getTitleAndBrand).toBeDefined();
+    expect(db.getDescriptionObject).toBeDefined();
+
+    db.getDescriptionObject(105).then((data) => {
+      expect(data[0]).toHaveProperty('description');
+      expect(data[0]).toHaveProperty('attributes');
+      expect(data[0]).toHaveProperty('details');
+      expect(data[0]).toHaveProperty('directions');
+      done();
+    });
+  });
+  it('should have methods for posting item docs', async (done) => {
+    expect(db.postDescriptionObject).toBeDefined();
+
+    db.postDescriptionObject(50, postData)
+      .then((data) => {
+        expect(data).toBeDefined();
         return db.getDescriptionObject(50);
       })
-      .then(doc => {
-        expect(doc[0].itemId).toBe('50');
-        expect(doc[0]).toMatchObject(postData);
+      .then((doc) => {
+        expect(doc[0]).toMatchObject(postRes);
         done();
-      })
-    })
-    it('should have methods for putting item docs', async (done) => {
-      expect(db.putDescriptionObject).toBeDefined()
-      
-      db.postDescriptionObject(postData)
-      .then(results => {
+      });
+  });
+  it('should have methods for putting item docs', async (done) => {
+    expect(db.putDescriptionObject).toBeDefined();
+
+    db.postDescriptionObject(50, postData)
+      .then((results) => {
         expect(results).toBeDefined();
         return db.putDescriptionObject(50, putData);
       })
-      .then(doc => {
-        expect(doc.title).toBe('The Shining');
+      .then((doc) => {
+        return db.getDescriptionObject(50);
       })
-    })
-    it('should have methods for deleting item docs', async (done) => {
-      expect(db.deleteDescriptionObject).toBeDefined()
+      .then((newDoc) => {
+        expect(newDoc[0].description.title).toBe('The Shining');
+        expect(newDoc[0]).toMatchObject(putResult);
+        done();
+      });
+  });
+  it('should have methods for deleting item docs', async (done) => {
+    expect(db.deleteDescriptionObject).toBeDefined();
 
-    db.postDescriptionObject(postData)
-      .then(results => {
+    db.postDescriptionObject(50, postData)
+      .then((results) => {
         return db.getDescriptionObject('50');
       })
-      .then(doc => {
+      .then((doc) => {
         expect(doc[0]).toBeDefined();
         return db.deleteDescriptionObject('50');
       })
-      .then(deleted => {
-        expect(deleted.itemId).toBe('50');
+      .then((deleted) => {
+        expect(deleted.deletedCount).toEqual(1);
         return db.getDescriptionObject('50');
       })
-      .then(erasedDoc => {
+      .then((erasedDoc) => {
+        expect(erasedDoc.length).toEqual(0);
         expect(erasedDoc[0]).toBeUndefined();
         done();
-      })
-  })
-})
+      });
+  });
+});
