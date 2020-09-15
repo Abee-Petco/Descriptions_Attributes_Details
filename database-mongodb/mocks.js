9010730f789d52c354d7cec3f141e5528bc7fbc2
Sam Johnson <samj@iconcollective.com>
2020-09-15 01:32:10 -0500

    moved mock data to separate file

---
+++ database-mongodb/mocks.js
@@ -0,0 +1,76 @@
+module.exports.postData = {
+  itemId: 50,
+  description: {
+    title: 'Dead Astronauts',
+    description: 'Once upon a time in the city of Los Angeles',
+    SKU: 6666666,
+    primaryBrand: 'Gucci',
+    daysToShip: 'Ships in 365 business days',
+  },
+  directions: {
+    directions: 'A left on Morose and right on Texas',
+  },
+  attributes: {
+    primaryColor: 'coral',
+    material: 'Melted',
+    length: '7 IN',
+    width: '7 IN',
+  },
+  details: {
+    additionalDetails: 'Loves long walks on the beach',
+  },
+};
+
+module.exports.postRes = {
+  description: {
+    title: 'Dead Astronauts',
+    description: 'Once upon a time in the city of Los Angeles',
+    SKU: 6666666,
+    primaryBrand: 'Gucci',
+    daysToShip: 'Ships in 365 business days',
+  },
+  directions: {
+    directions: 'A left on Morose and right on Texas',
+  },
+  attributes: {
+    primaryColor: 'coral',
+    material: 'Melted',
+    length: '7 IN',
+    width: '7 IN',
+  },
+  details: {
+    additionalDetails: 'Loves long walks on the beach',
+  },
+};
+
+module.exports.putData = {
+  description: {
+    title: 'The Shining',
+    description: 'Once upon a time in the city of Los Angeles',
+    SKU: 6666666,
+    primaryBrand: 'Gucci',
+    daysToShip: 'Ships in 365 business days',
+  },
+};
+
+module.exports.putResult = {
+  description: {
+    title: 'The Shining',
+    description: 'Once upon a time in the city of Los Angeles',
+    SKU: 6666666,
+    primaryBrand: 'Gucci',
+    daysToShip: 'Ships in 365 business days',
+  },
+  directions: {
+    directions: 'A left on Morose and right on Texas',
+  },
+  attributes: {
+    primaryColor: 'coral',
+    material: 'Melted',
+    length: '7 IN',
+    width: '7 IN',
+  },
+  details: {
+    additionalDetails: 'Loves long walks on the beach',
+  },
+};
