a73e7cf66ab775602b2f512964164795d1baf846
Sam Johnson <samj@iconcollective.com>
2020-09-15 01:35:37 -0500

    moved endpoint test mocks to separate file

--- server.spec.js
+++ server.spec.js
@@ -1,52 +1,7 @@
 const supertest = require('supertest');
 const app = require('./server.js');
 const request = supertest(app);
-
-const postData = {
-  itemId: '66',
-  description: {
-    title: 'Fear and Loathing',
-    description: 'Once upon a time in the city of Los Angeles',
-    SKU: '6666666',
-    'Primary Brand': 'Gucci',
-    daysToShip: 'Ships in 365 business days',
-  },
-  directions: {
-    directions: 'Just a toot past the watering hole',
-  },
-  attributes: {
-    primaryColor: 'coral',
-    material: 'Melted',
-    length: '7 IN',
-    width: '7 IN',
-  },
-  details: {
-    additionalDetails: 'Loves long walks on the beach',
-  },
-};
-
-const putData = {
-  itemId: '66',
-  description: {
-    title: 'Dead Astronauts',
-    description: 'Once upon a time in the city of Los Angeles',
-    SKU: '6666666',
-    'Primary Brand': 'Gucci',
-    daysToShip: 'Ships in 365 business days',
-  },
-  directions: {
-    directions: 'Just a toot past the watering hole',
-  },
-  attributes: {
-    primaryColor: 'coral',
-    material: 'Melted',
-    length: '7 IN',
-    width: '7 IN',
-  },
-  details: {
-    additionalDetails: 'Loves long walks on the beach',
-  },
-};
+const { postData, putData } = require('./serverMocks.js')
 
 describe('CRUD Routes', () => {
   it('should respond with descriptions data at itemIds 100-200', async (done) => {
