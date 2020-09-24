1ccfbb4e5d45d51130f6cb3d7f595a9d6793126b
Sam Johnson <samj@iconcollective.com>
2020-09-24 18:37:58 -0500

    loading current at hydrate and refactored out componentDidMount

--- client/src/render.jsx
+++ client/src/render.jsx
@@ -2,9 +2,4 @@ import DescriptionService from './index.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-console.log(window.__initData__)
-
-var bullets = window.__initData__.description.description.split('. ');
-window.__initData__.description.description = bullets;
-
-ReactDOM.hydrate(<DescriptionService initData={window.__initData__}/>, document.getElementById("description"));
+ReactDOM.hydrate(<DescriptionService initData={window.__initData__} initCurrent={'descriptionB'}/>, document.getElementById("description"));
