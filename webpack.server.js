06338dd426030f503148b7f5776b6db487330f0a
Sam Johnson <samj@iconcollective.com>
2020-09-24 19:10:23 -0500

    combined webpack configs

--- webpack.server.js
+++ webpack.server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
+const ReactConfig = {
   entry: './server.js',
 
   target: 'node',
@@ -27,3 +27,39 @@ module.exports = {
     ]
   }
 };
+
+var CompressionPlugin = require('compression-webpack-plugin');
+
+const NodeConfig = {
+  mode: 'production',
+  entry: __dirname + '/client/src/render.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CompressionPlugin({
+      filename: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$/,
+      threshold: 5000,
+      minRatio: 0.8
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/public'
+  }
+};
+
+module.exports = [ReactConfig, NodeConfig];
