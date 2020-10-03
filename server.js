c6320a5e4909a8ee37fd593aaa1fcf5b2babea8d
Sam Johnson <samj@iconcollective.com>
2020-10-03 15:43:50 -0500

    simplified header middleware

--- server.js
+++ server.js
@@ -24,26 +24,27 @@ app.use((req, res, next) => {
   // const address3 = 'http://localhost';
 
   //deployed address
-  var address = 'http://ec2-3-132-55-48.us-east-2.compute.amazonaws.com'; //me
+  // var address = 'http://ec2-3-132-55-48.us-east-2.compute.amazonaws.com'; //me
   // var address2 = 'http://54.183.137.155'; // nick
   var address3 = 'http://ec2-3-132-55-48.us-east-2.compute.amazonaws.com'; // kate
 
   const { referer } = req.headers;
-  if (referer) {
-    if (req.headers.referer.includes(`${address2}:3004`)) {
-      res.header('Access-Control-Allow-Origin', `${address2}:3004`); //recommended
-    } else if (req.headers.referer.includes('3005')) {
-      res.header('Access-Control-Allow-Origin', `${address}:3005`); //title/price
-    } else if (req.headers.referer.includes(`${address3}:3006`)) {
-      res.header('Access-Control-Allow-Origin', `${address3}:3006`); //deliver
-    } else if (req.headers.referer.includes(`${address}:3000`)) {
-      res.header('Access-Control-Allow-Origin', `${address}:3000`); //my proxy
-    } else if (req.headers.referer.includes(`${address2}:3000`)) {
-      res.header('Access-Control-Allow-Origin', `${address2}:3000`); //nick's proxy
-    } else if (req.headers.referer.includes(`${address3}`)) {
-      res.header('Access-Control-Allow-Origin', `${address3}`); //kate's proxy
-    }
-  }
+  // if (referer) {
+  //   if (req.headers.referer.includes(`${address2}:3004`)) {
+  //     res.header('Access-Control-Allow-Origin', `${address2}:3004`); //recommended
+  //   } else if (req.headers.referer.includes('3005')) {
+  //     res.header('Access-Control-Allow-Origin', `${address}:3005`); //title/price
+  //   } else if (req.headers.referer.includes(`${address3}:3006`)) {
+  //     res.header('Access-Control-Allow-Origin', `${address3}:3006`); //deliver
+  //   } else if (req.headers.referer.includes(`${address}:3000`)) {
+  //     res.header('Access-Control-Allow-Origin', `${address}:3000`); //my proxy
+  //   } else if (req.headers.referer.includes(`${address2}:3000`)) {
+  //     res.header('Access-Control-Allow-Origin', `${address2}:3000`); //nick's proxy
+  //   } else if (req.headers.referer.includes(`${address3}`)) {
+  //     res.header('Access-Control-Allow-Origin', `${address3}`); //kate's proxy
+  //   }
+  // }
+  res.header('Access-Control-Allow-Origin', `${address3}`);
   next();
 });
 
