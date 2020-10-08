8cdbf5465171003d20638fd464409dad2f44140d
Sam Johnson <samj@iconcollective.com>
2020-10-08 16:24:40 -0500

    added morgan back in for debugging

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const axios = require('axios');
 const app = express();
 
 app.use(express.json());
-// app.use(morgan('dev'));
+app.use(morgan('combined'));
 
 //crossorigin permission for 3000, 3004, 3005 and 3006
 app.use((req, res, next) => {
